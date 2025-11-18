6a76d03bc3a5927ba3702772dd21751f5ae88d73
zereim <wtruskolaski@gmail.com>
2025-11-18 03:42:14 -0500

    Created a function for cleaning

--- main.R
+++ main.R
@@ -7,44 +7,34 @@ library(broom)
 data <- clean_names(read_csv("2025 County Health Rankings NC.csv"))
 
 # creating a function that will do all of the data cleaning for me
-clean_import <- function(df, replace_header_boolean, min_character = 5) {
-  # first clean the header names 
-  clean_names(df)
+clean_import <- function(df, replace_header_bool = FALSE, min_character = 5) {
+  # create a variable and clean the dataframe
+  df_cleaned <- janitor::clean_names(df)
   # if we want to replace the header 
-  if (replace_header_boolean == TRUE && missing(min_characters) == FALSE ) {
+  if (replace_header_bool == TRUE) {
+    # store columns under the min char count
+    short_cols <- nchar(names(df_cleaned)) < min_character
+    # take the 1st row 
+    header_row <- as.character(df_cleaned[1, ])
+    # take the columns with short names and replace their values with the ones 
+    # from the first row
+    names(df_cleaned)[short_cols] <- header_row[short_cols]
+    # appending the non-short columns 
+    names(df_cleaned)[!short_cols] <- paste0(
+      names(df_cleaned)[!short_cols], 
+      " - ", 
+      header_row[!short_cols]
+    )
+    # removing the 1st row
+    df_cleaned <- df_cleaned[-1, ]
     
-    short_cols <- nchar(names(df)) < min_characters
-    
-    names(df)[short_cols] <- as.character(df[1, short_cols])
-    
-    
-  } else if (replace_header_boolean == TRUE) {
-    
-  } else if (missing(min_characters)) {
-    
-  }
-  
+  } 
+  # clean the names again
+  names(df_cleaned) <- janitor::clean_names(names(df_cleaned))
+  # return the final product 
+  return(df_cleaned)
 }
 
-# taking the auto-replaced header strings
-short_cols <- nchar(names(data)) < 5
-
-# now replacing with new column names that were in the 1st row
-names(data)[short_cols] <- as.character(data[1, short_cols])
-
-# taking all of the strings that were left out of short_cols and
-# adding the first column title values to them after a - 
-names(data)[!short_cols] <- paste0(
-  names(data)[!short_cols], 
-  " - ", 
-  as.character(data[1, !short_cols])
-)
-
-# removing the 1st column
-data <- data[-1, ]
-
-# cleaning names again 
-data <- clean_names(data)
 
 # run a linear regression
 model_commute <- lm(
